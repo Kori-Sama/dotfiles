f173f56afc951ef5a7c484555ab3ee47b927f0e0
KoriSama <korisamade@gmail.com>
2026-05-06 20:15:39 +0800

    cachy commit

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -3,9 +3,12 @@ alias vi=nvim
 alias vim=nvim
 alias pn=pnpm
 
-alias cls="clear"
 alias ls="eza --icons"
 alias ll="eza -la --icons -I .DS_Store"
 alias cat="bat -pp"
 
 alias lg="lazygit"
+
+if command -q podman
+    alias docker='podman'
+end
