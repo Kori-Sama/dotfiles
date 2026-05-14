2f6ad53974a484b05cda3c409d312a96af65e678
KoriSama <korisamade@gmail.com>
2026-05-14 14:05:42 +0800

    cachy commit: add fastfetch

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -6,8 +6,10 @@ alias pn=pnpm
 alias ls="eza --icons"
 alias ll="eza -la --icons -I .DS_Store"
 alias cat="bat -pp"
+alias tldr="tldr -L zh"
 
 alias lg="lazygit"
+alias zed="zeditor"
 
 if command -q podman
     alias docker='podman'
--- dot_config/fish/conf.d/variables.fish
+++ dot_config/fish/conf.d/variables.fish
@@ -1,2 +1,11 @@
 set -gx EDITOR nvim
-set -x SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
+
+set -x SSH_AUTH_SOCK ( 
+    if test -e "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
+        echo "$HOME/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock"
+    else if test -e "$HOME/.bitwarden-ssh-agent.sock"
+        echo "$HOME/.bitwarden-ssh-agent.sock"
+    else
+        echo ""
+    end
+)
